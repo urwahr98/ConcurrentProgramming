a437c1f1d84e68269fc04d877423167642e4d6e7
urwah <urwah@LAPTOP-DT3KPAB2.mshome.net>
2020-06-11 17:18:20 +0800

    clarify some comments

--- ConcurrentProgramming/src/concurrentprogramming/Pairing.java
+++ ConcurrentProgramming/src/concurrentprogramming/Pairing.java
@@ -37,27 +37,27 @@ public class Pairing implements Runnable{
         
         //looping while stopCriteria has not been reached
         while(!stopCriteria.get()){
+            //randomly get a point 
             int first = ran.nextInt(pointArr.size());
-            //Get a random point from the arraylist
-            //if paired, +1 to fail number of the thread
-            //if not paired yet, change to paired
-            //get another random point
-            //if not paired yet, change to paired
-            // +1 to succesful number of the thread
-            //if paired, +1 to failure number of the thread
-            //find another point to pair with
-            if (pointArr.get(first).isPair.get()){              
+            //check if paired or not
+            if (pointArr.get(first).isPair.get()){
+                //paired -> increase thread failure number by 1
                 if (fail.containsKey(String.valueOf(Thread.currentThread().getId()))){
+                    //if the thread has already existed in the hashmap
                     fail.put(String.valueOf(Thread.currentThread().getId()),fail.get(String.valueOf(Thread.currentThread().getId()))+1);
+                    //check if failure number reached 20
                     if(fail.get(String.valueOf(Thread.currentThread().getId())) >= 20){
                             stopCriteria.set(true);
                         }
                 }else{
+                    //if the thread is not in the hashmap, put it as key with value 1
                     fail.put(String.valueOf(Thread.currentThread().getId()),1);
                 }
             }else {
+                //set the status of the first point as pair
                 pointArr.get(first).isPair.set(true);
                 boolean loop = true;
+                //loop to find an unpaired second point
                 while(loop || !stopCriteria.get()){
                     //added sleep to give chance to other thread to work
                     try {
@@ -65,6 +65,8 @@ public class Pairing implements Runnable{
                     } catch (InterruptedException ex) {
                         System.out.println("Sleep Interrupted");
                     }
+                    //randomly get a second point
+                    //same algorithm as above
                     int second = ran.nextInt(pointArr.size());
                     if (pointArr.get(second).isPair.get()){                          
                         if (fail.containsKey(String.valueOf(Thread.currentThread().getId()))){
@@ -77,8 +79,11 @@ public class Pairing implements Runnable{
                             fail.put(String.valueOf(Thread.currentThread().getId()),1);
                     }
                     }else{
+                        // set the second point's status as paired
                         pointArr.get(second).isPair.set(true);
 //                        System.out.println("Thread "+Thread.currentThread().getId()+" created an edge!");
+                        //increase thread successful number by 1
+                        //break out of the loop
                         if (success.containsKey(String.valueOf(Thread.currentThread().getId()))){
                             success.put(String.valueOf(Thread.currentThread().getId()),success.get(String.valueOf(Thread.currentThread().getId()))+1);
                             loop=false;
